736f0bc5b63135dde2916f121b0c6e15e73e199c
Greg <hzy810225@gmail.com>
2018-05-07 11:43:48 +0800

    Permission-1-not good

--- OpenPermission-1/app/src/main/java/com/example/user/MainActivity.kt
+++ OpenPermission-1/app/src/main/java/com/example/user/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.user
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //  檢查權限
-        Permission().askMicrophonePermission()
+        //  this@MainActivity 是這個 MainActivity 的環境
+        //  進入 Permission().askMicrophonePermission
+        Log.i("Message","即將問權限")
+        Permission().askMicrophonePermission(this@MainActivity)
+        Permission().askCameraPermission(this@MainActivity)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        //  進入 Permission().onRequestPermissionsResult
+        Permission().onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
--- OpenPermission-1/app/src/main/java/com/example/user/Permission.kt
+++ OpenPermission-1/app/src/main/java/com/example/user/Permission.kt
@@ -1,17 +1,52 @@
 package com.example.user
 
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import android.support.v4.app.ActivityCompat
+import android.util.Log
 
 
 class Permission{
-    fun askMicrophonePermission(){
 
-        val userMicrophonePermissionAgreeCode = 1
-        val currentMicrophonePermission = ActivityCompat.checkSelfPermission(_________,android.Manifest.permission.RECORD_AUDIO)
-        if(currentMicrophonePermission!=PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(_______________, arrayOf(android.Manifest.permission.RECORD_AUDIO), userMicrophonePermissionAgreeCode)
+    private val tag = "Permission message"
+
+
+    private val userMicrophonePermissionAgreeCode = 1
+    private val userCameraPermissionAgreeCode = 2
+
+    fun askMicrophonePermission(context: Context){
+        val currentMicrophonePermission = ActivityCompat.checkSelfPermission(context,android.Manifest.permission.RECORD_AUDIO)
+        if(currentMicrophonePermission != PackageManager.PERMISSION_GRANTED)
+            //  進入 MainActivity 的 onRequestPermissionsResult
+            ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.RECORD_AUDIO), userMicrophonePermissionAgreeCode)
+    }
+
+    fun askCameraPermission(context: Context){
+        val currentCameraPermission = ActivityCompat.checkSelfPermission(context,android.Manifest.permission.CAMERA)
+        if(currentCameraPermission != PackageManager.PERMISSION_GRANTED)
+        //  進入 MainActivity 的 onRequestPermissionsResult
+            ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.CAMERA), userCameraPermissionAgreeCode)
+    }
+
+    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        //  看一下這是甚麼
+        Log.i(tag,permissions.toString())
+
+        when(requestCode){
+            userMicrophonePermissionAgreeCode -> {
+                if( grantResults[0] == PackageManager.PERMISSION_GRANTED )
+                    Log.i(tag,"Agree microphone permission")
+                else
+                    Log.i(tag,"Not agree microphone permission")
+            }
+            userCameraPermissionAgreeCode -> {
+                if( grantResults[0] == PackageManager.PERMISSION_GRANTED )
+                    Log.i(tag,"Agree camera permission")
+                else
+                    Log.i(tag,"Not agree camera permission")
+            }
         }
     }
 }
